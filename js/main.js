e174c007845b7bd04c4c3f2a3cbbed048a21f052
Andrey Sorokin <73.anir@gmail.com>
2021-08-20 02:02:17 +0400

    Finish project

--- js/main.js
+++ js/main.js
@@ -6,3 +6,10 @@ function changeBgColor(color) {
 	const sec = document.querySelector('.sec');
 	sec.style.background = color;
 }
+
+function menuToggle() {
+	const toggleMenu = document.querySelector('.toggle-menu');
+	const navigation = document.querySelector('.nav');
+	toggleMenu.classList.toggle('active');
+	navigation.classList.toggle('active');
+}
